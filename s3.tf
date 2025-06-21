d608a85746ab03b5af7093b4f0c878eb35b36599
Suhas097 <suhaslyagalwar2838@gmail.com>
2025-06-21 23:53:28 +0530

    dfdd

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 # Create an S3 Bucket
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "swwgysuhas-bucket-terraform"
+  bucket = "swwgysuhas-bucket-terraform12"
 
   # Enable versioning
   versioning {
